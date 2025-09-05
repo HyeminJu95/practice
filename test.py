f335928c0a2d43cb708d3e4eea51a3ebc666297f
HyeminJu95 <dpgpfkdkeldi@gmail.com>
2025-09-05 14:25:40 +0900

    feat: add numpy/pandas/matplotlib example

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("jj2")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+arr = np.array([1, 2, 3])
+print("numpy array:", arr)
+
+df = pd.DataFrame({"col1": [1, 2, 3], "col2": [4, 5, 6]})
+print("pandas dataframe:\n", df)
+
+plt.plot(df["col1"], df["col2"])
+plt.title("Practice Plot")
+plt.savefig("plot.png")
+
